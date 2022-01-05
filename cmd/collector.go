80b7a4daf87a2bbe172d1fcea311133b03942c7a
yihongzhi <yihongzhi@megvii.com>
2022-01-05 13:59:20 +0800

    load config

--- cmd/collector.go
+++ cmd/collector.go
@@ -3,12 +3,43 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
+	"path"
 )
 
 var collectorCmd = &cobra.Command{
 	Use:   "collector",
 	Short: "collector",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hugo Static Site Generator v0.9 -- HEAD")
+
 	},
+}
+
+var cfgFile string
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	collectorCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is config/collector.yaml)")
+	rootCmd.AddCommand(collectorCmd)
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		pwd, err := os.Getwd()
+		cobra.CheckErr(err)
+		viper.AddConfigPath(path.Join(pwd, "config"))
+		viper.SetConfigType("yaml")
+		viper.SetConfigName("collector")
+	}
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stdout, "Using config file:", viper.ConfigFileUsed())
+	} else {
+		fmt.Fprintln(os.Stderr, "reda config file error:", err)
+	}
 }
--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,8 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-	"os"
 )
-
-var cfgFile string
 
 var rootCmd = &cobra.Command{
 	Use:   "log-kit",
@@ -16,29 +11,4 @@ var rootCmd = &cobra.Command{
 
 func Execute() error {
 	return rootCmd.Execute()
-}
-
-func init() {
-	cobra.OnInitialize(initConfig)
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/config.yaml)")
-	rootCmd.AddCommand(collectorCmd)
-}
-
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-		// Search config in home directory with name ".cobra" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName("config")
-	}
-	viper.AutomaticEnv()
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
 }
