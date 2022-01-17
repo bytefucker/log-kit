22f7932610771a48556f048c570a18ca9f088251
yihongzhi <yihongzhi@megvii.com>
2022-01-17 15:59:53 +0800

    es

--- analyzer/parser/log_parser.go
+++ analyzer/parser/log_parser.go
@@ -3,15 +3,15 @@ package parser
 import "time"
 
 type LogContent struct {
-	Time      time.Time
-	Level     string
-	TxId      string
-	SpanId    string
-	AppId     string
-	Host      string
-	ParseTime time.Time
-	Field     map[string]string
-	Content   string
+	Time      time.Time         `json:"time"`
+	Level     string            `json:"level"`
+	TxId      string            `json:"tx_id"`
+	SpanId    string            `json:"span_id"`
+	AppId     string            `json:"app_id"`
+	Host      string            `json:"host"`
+	ParseTime time.Time         `json:"parse_time"`
+	Field     map[string]string `json:"field"`
+	Content   string            `json:"content"`
 }
 
 // LogParser 日志解析器
